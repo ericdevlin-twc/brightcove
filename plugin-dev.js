063ac06d1761a72a9e90aed730b195b3edb66ddd
Eric <ericdevlin@me.com>
2017-05-01 16:05:15 -0400

    init commit

---
+++ plugin-dev.js
@@ -0,0 +1,9 @@
+console.log('hello');
+
+videojs.plugin('pluginDev', function(options) {
+  var player = this,
+  overlay = document.createElement('p');
+  overlay.className = 'vjs-overlay';
+  overlay.innerHTML = options.overlayText;
+  player.el().appendChild(overlay);
+});
