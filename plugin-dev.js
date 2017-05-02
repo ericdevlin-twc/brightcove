d67df3d0237f8ffc162f7ac81d2041bee3741c84
ericdevlin-twc <eric.devlin.contractor@weather.com>
2017-05-02 10:51:57 -0400

    testing js

--- plugin-dev.js
+++ plugin-dev.js
@@ -1,12 +1,8 @@
-$(document).ready() {
 
   videojs.plugin('pluginDev', function(options) {
-    var player = this;
-    // overlay = document.createElement('p');
-    // overlay.className = 'vjs-overlay';
-    // overlay.innerHTML = options.overlayText;
-    // player.el().appendChild(overlay);
-    // myPlayer.muted(true);
+    var player = this,
+    overlay = document.createElement('p');
+    overlay.className = 'vjs-overlay';
+    overlay.innerHTML = options.overlayText;
+    player.el().appendChild(overlay);
   });
-
-}
